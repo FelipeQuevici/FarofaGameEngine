e0e81f8f349f9408ee8d61301f64f56bf12362a6
Bcvolpe <bcvolpe@gmail.com>
2016-05-23 21:16:04 -0700

    Adrenaline update

--- KiomeGame/Scripts/Components/PlayerStatsComponent.js
+++ KiomeGame/Scripts/Components/PlayerStatsComponent.js
@@ -6,13 +6,19 @@ PlayerStatsComponent.inheritsFrom(Component);
 
 function PlayerStatsComponent(parent) {
     var currentMoney = 0;
-
+    var playerController;
+    
     this.onCreate = function (parent) {
         this.parent = parent;
         this.maxHealth = 6;
         this.currentHealth = this.maxHealth;
+        this.adrenaline = 10;
+        this.adrenalineSpeed = 0.2;
+        this.moveSpeed = 200;
         currentMoney = 0;
         EventCenterInstance.getInstance().subscribeEvent("enemyDied", enemyDied);
+        playerController = this.parent.getComponent("playerController");
+        playerController.setMoveSpeed(this.moveSpeed);
     };
 
     this.removeLife = function (amount) {
@@ -22,13 +28,24 @@ function PlayerStatsComponent(parent) {
 
     this.getCurrentMoney = function () {
         return currentMoney;
-    };
+    };       
 
     var enemyDied = function (args) {
         var enemy = args["enemy"];
         var value = enemy.getComponent("stats").money;
         currentMoney += value;
+        this.adrenaline += 1;
     };
+    
+    this.onUpdate = function (deltaTime) {
+    	this.adrenaline -= this.adrenalineSpeed * deltaTime;    
+    	if(this.adrenaline <= 0){
+    		this.adrenaline = 0;
+    		playerController.setMoveSpeed(this.moveSpeed / 10);
+    	}else{
+    		playerController.setMoveSpeed(this.moveSpeed);
+    	}
+    };    
 
     this.onCreate(parent);
 }
