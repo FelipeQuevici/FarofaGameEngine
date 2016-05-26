41e7248552c0749794bf678e83b3437dcfa1afc3
FelipeQuevici <felipe_quevici@hotmail.com>
2016-05-26 15:50:11 -0700

    Something

---
+++ FarofaEngine/Menu/Menu.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Felipe on 26/05/2016.
+ */
+
+MenuGameObject.inheritsFrom(GameObject);
+
+function MenuGameObject(scene) {
+    
+}
---
+++ KiomeGame/Scripts/Scenes/MenuScene.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Felipe on 26/05/2016.
+ */
+
+MenuScene.inheritsFrom(Scene);
+
+function MenuScene() {
+    
+}
